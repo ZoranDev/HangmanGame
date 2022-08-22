46d367b9af7aa55ef2c4c528cd070ad2d6e4cf3d
ZoranDEV <zoranpavicevic098@gmail.com>
2022-08-22 13:00:02 +0200

    add letters and some style to them

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
+import Letter from "./components/Letter";
+
 function App() {
+  const letters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
   return (
     <div className="w-full h-screen bg-slate-700 flex flex-col justify-center items-center">
       {/* Hangman section */}
@@ -7,8 +38,10 @@ function App() {
       </div>
 
       {/* Letters section */}
-      <div>
-        <h2>Here put letters</h2>
+      <div className="w-3/4 flex justify-center items-center flex-wrap">
+        {letters.map((letter, index) => (
+          <Letter key={index} letter={letter} />
+        ))}
       </div>
     </div>
   );
---
+++ src/components/Letter.js
@@ -0,0 +1,9 @@
+const Letter = ({ letter }) => {
+  return (
+    <div className="w-12 h-12 bg-transparent text-white mr-2 mb-2 text-lg flex justify-center items-center border-2 border-white cursor-pointer">
+      {letter}
+    </div>
+  );
+};
+
+export default Letter;
